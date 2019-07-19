be46f7f4de8d8fe681db4f26db015354d4f72f34
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2019-07-19 10:30:04 +0200

    update board's state

--- src/index.js
+++ src/index.js
@@ -9,21 +9,33 @@ class Square extends React.Component {
       value: null,
     };
   }
+
   render() {
     return (
       <button
       className="square"
-      onClick={()=>this.setState({value:'X'})}
+      onClick={() => this.props.onClick()}
       >
-        {this.state.value}
+        {this.props.value}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
+  constructor(props){
+    super(props);
+
+      this.state = {
+        squares: Array(9).fill(null),
+      };
+
+  }
   renderSquare(i) {
-    return <Square value={i}/>;
+    return <Square
+    value={this.state.squares[i]}
+    onClick={() => this.handleClick(i)}
+    />;
   }
 
   render() {
