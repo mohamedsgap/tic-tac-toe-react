d41922d1b7d218991998509489bed21babb2cb64
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2019-07-21 03:12:18 +0200

    taking turns

--- src/index.js
+++ src/index.js
@@ -42,14 +42,19 @@ class Board extends React.Component {
 
       this.state = {
         squares: Array(9).fill(null),
+        xIsNext: true,
       };
 
   }
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares:squares});
+    squares[i] = this.state.xIsNext? 'X' : 'O';
+    this.setState({
+      squares:squares,
+      xIsNext: !this.state.xIsNext,
+
+    });
   }
   renderSquare(i) {
     return <Square
@@ -59,7 +64,7 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' + (this.state.xIsNext? 'X' : 'O');
 
     return (
       <div>
