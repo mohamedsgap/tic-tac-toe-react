e2ea0bd59e6c6dce9abed7d3d7277fd1623333a0
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2019-07-21 03:28:05 +0200

    declaring a winner

--- src/index.js
+++ src/index.js
@@ -49,6 +49,9 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice();
+    if(  calculateWinner(squares) || squares[i] ){
+      return;
+    }
     squares[i] = this.state.xIsNext? 'X' : 'O';
     this.setState({
       squares:squares,
@@ -64,7 +67,14 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: ' + (this.state.xIsNext? 'X' : 'O');
+    ////const status = 'Next player: ' + (this.state.xIsNext? 'X' : 'O');
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if(winner){
+      status = 'Winner ' + winner;
+    } else {
+      status = 'Next Player is ' + (this.state.xIsNext? 'X' : 'O');
+    }
 
     return (
       <div>
