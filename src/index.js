d49e2c0987abb328029ab88405c94b9234b97a02
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2019-07-21 03:01:02 +0200

    convert to func. component

--- src/index.js
+++ src/index.js
@@ -2,16 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+/*
 class Square extends React.Component {
 // the constructor is no longer keeps track of game's state
-/*
+
   constructor(props){
     super(props);
     this.state = {
       value: null,
     };
   }
-*/
+
   render() {
     return (
       <button
@@ -23,6 +24,17 @@ class Square extends React.Component {
     );
   }
 }
+*/
+// convert square Component to function component
+
+function Square(props) {
+  return(
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
+
 
 class Board extends React.Component {
   constructor(props){
