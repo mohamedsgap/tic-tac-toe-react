0fec9fdaed2f3284733629c7a98b93888f985633
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2019-07-22 17:14:35 +0200

    lifting state up

--- src/index.js
+++ src/index.js
@@ -37,6 +37,8 @@ function Square(props) {
 
 
 class Board extends React.Component {
+// removing the constructor for lifting the state up again
+  /*
   constructor(props){
     super(props);
 
@@ -46,7 +48,7 @@ class Board extends React.Component {
       };
 
   }
-
+*/
   handleClick(i) {
     const squares = this.state.squares.slice();
     if(  calculateWinner(squares) || squares[i] ){
@@ -61,8 +63,8 @@ class Board extends React.Component {
   }
   renderSquare(i) {
     return <Square
-    value={this.state.squares[i]}
-    onClick={() => this.handleClick(i)}
+    value={this.props.squares[i]}
+    onClick={() => this.props.onClick(i)}
     />;
   }
 
@@ -110,13 +112,26 @@ class Game extends React.Component {
     };
   }
   render() {
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const winner = calculateWinner(current.squares);
+
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board
+          squares={current.squares}
+          onClick={(i) => this.handleClick(i)}
+          />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
+          <div>{status}</div>
           <ol>{/* TODO */}</ol>
         </div>
       </div>
