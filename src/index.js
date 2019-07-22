da4cfcd4cf3d0512adb7f128acc6d08275fb0a85
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2019-07-22 17:44:09 +0200

    show past moves

--- src/index.js
+++ src/index.js
@@ -131,10 +131,21 @@ class Game extends React.Component {
     });
   }
   render() {
+
+
     const history = this.state.history;
     const current = history[history.length - 1];
     const winner = calculateWinner(current.squares);
-
+    const moves = history.map((step, move) => {
+      const desc = move ?
+        'Go to move #' + move :
+        'Go to game start';
+      return (
+        <li>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
     let status;
     if (winner) {
       status = 'Winner: ' + winner;
@@ -151,7 +162,7 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
